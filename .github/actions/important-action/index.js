61459db8e720d4d0e71a10ba494160650685ca11
HERO <ahmadsiddique120@gmail.com>
2025-11-28 23:39:43 +0500

    ahsdwqe

--- .github/actions/important-action/index.js
+++ .github/actions/important-action/index.js
@@ -1,9 +1,19 @@
 const core = require('@actions/core');
+const exec = require('@actions/exec');
 
 async function run() {
   try {
-    const message = "This is output from JS action!";
-    core.setOutput("result", message);
+    const bucket = core.getInput('bucket');
+    const region = core.getInput('region');
+    const dist = core.getInput('dist-folder');
+
+    console.log("Deploying to S3...");
+    
+    await exec.exec(`aws s3 sync ${dist} s3://${bucket} --delete`);
+
+    const siteUrl = `http://${bucket}.s3-website-${region}.amazonaws.com`;
+
+    core.setOutput("site-url", siteUrl);
   } catch (error) {
     core.setFailed(error.message);
   }
